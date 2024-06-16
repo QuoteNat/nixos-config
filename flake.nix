106621746aec83af4e6cdfc38d4c7ec4ad1ecd9d
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2024-06-15 20:58:43 -0500

    Add unstable branch to flake inputs

--- flake.nix
+++ flake.nix
@@ -3,23 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+		nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs: 
+	let
+		system = "x86_64-linux";
+		overlay-unstable = final: prev: {
+			 unstable = import nixpkgs-unstable {
+			   inherit system;
+			   config.allowUnfree = true;
+			 };
+		};
+	in
+	{
     nixosConfigurations = {
-	test-vm = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs;};
-          modules = [
-            ./hosts/test-vm/configuration.nix
-            # inputs.home-manager.nixosModules.default
-          ];
-        };
-	home = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs;};
-          modules = [
-            ./hosts/home/configuration.nix
-          ];
-        };
-     };
+		test-vm = nixpkgs.lib.nixosSystem {
+						specialArgs = {inherit inputs;};
+						modules = [
+							./hosts/test-vm/configuration.nix
+							# inputs.home-manager.nixosModules.default
+						];
+					};
+		home = nixpkgs.lib.nixosSystem {
+						specialArgs = {inherit inputs;};
+						modules = [
+							({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+							./hosts/home/configuration.nix
+						];
+		};
+		};
   };
 }
