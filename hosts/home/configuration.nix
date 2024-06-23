5b1418eae469a0885327e500dc36dac54212aa83
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2024-06-23 09:46:46 -0500

    Update to the linuxPackages_latest kernel

--- hosts/home/configuration.nix
+++ hosts/home/configuration.nix
@@ -21,6 +21,8 @@
   office.enable = true;
 	flatpak.enable = true;
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
